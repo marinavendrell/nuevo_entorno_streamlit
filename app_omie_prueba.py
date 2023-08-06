4d62c0ed9292738a84c3461aab97f2ae4375eff0
Marina Vendrell <84765188+marinavendrell@users.noreply.github.com>
2023-08-06 14:26:46 +0200

    Update app_omie_prueba.py

--- app_omie_prueba.py
+++ app_omie_prueba.py
@@ -26,8 +26,8 @@ fecha_24_horas_futuro = fecha_hora_actual + desplazamiento
 # This can take time, it is downloading the files from the website..
 df = OMIEMarginalPriceFileImporter(date_ini=fecha_hora_actual, date_end=fecha_24_horas_futuro).read_to_dataframe(verbose=True)
 
-df.sort_values(by='DATE', axis=0, inplace=True)
-df = df[df.CONCEPT == 'PRICE_SP']
+#df.sort_values(by='DATE', axis=0, inplace=True)
+#df = df[df.CONCEPT == 'PRICE_SP']
 
 
 st.dataframe(df)
