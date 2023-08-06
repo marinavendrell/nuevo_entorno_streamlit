27d97793baa833e519d0bd7acbed690577a40198
Marina Vendrell <84765188+marinavendrell@users.noreply.github.com>
2023-08-06 15:20:30 +0200

    Update app_omie_prueba.py

--- app_omie_prueba.py
+++ app_omie_prueba.py
@@ -24,11 +24,15 @@ fecha_24_horas_futuro = fecha_hora_actual + desplazamiento
 
 
 # This can take time, it is downloading the files from the website..
-df = OMIEMarginalPriceFileImporter(date_ini=fecha_hora_actual, date_end=fecha_24_horas_futuro).read_to_dataframe(verbose=True)
+#df = OMIEMarginalPriceFileImporter(date_ini=fecha_hora_actual, date_end=fecha_24_horas_futuro).read_to_dataframe(verbose=True)
 
 #df.sort_values(by='DATE', axis=0, inplace=True)
 #df = df[df.CONCEPT == 'PRICE_SP']
 
+url = "https://www.omie.es/sites/default/files/dados/AGNO_2023/MES_08/TXT/INT_PBC_EV_H_1_06_08_2023_06_08_2023.TXT"
+
+# Leer los datos desde la URL y crear un DataFrame
+df = pd.read_csv(url, sep=";", encoding="ISO-8859-1", skiprows=2)
 
 st.dataframe(df)
 
